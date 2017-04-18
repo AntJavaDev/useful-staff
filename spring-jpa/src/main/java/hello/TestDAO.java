7ca9f60c505710f655f5200d7701a404b77c7fb0
AntJavaDev <antcsdev@gmail.com>
2017-04-18 20:38:21 +0300

    first proj
    added code

---
+++ spring-jpa/src/main/java/hello/TestDAO.java
@@ -0,0 +1,27 @@
+package hello;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TestDAO 
+{
+
+	private static final Logger log = LoggerFactory.getLogger(TestDAO.class);
+	 @Autowired
+	 private EntityManager entityManager;
+	 
+
+	 @PostConstruct
+	 public void test()
+	 {
+		 log.info(entityManager.toString());
+		 System.out.println(entityManager);
+	 }
+	 
+}
